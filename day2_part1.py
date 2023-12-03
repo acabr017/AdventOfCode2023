7868776a3ce36864b7be34573fcdc8c553153358
acabr017 <arielcabrera3@gmail.com>
2023-12-02 22:50:46 -0500

    Day2, both parts completed. Much easier than day1

---
+++ day2_part1.py
@@ -0,0 +1,23 @@
+limits = {"red": 12, "green": 13, "blue": 14}
+id_sum = 0
+data = "day2_example_data.txt"
+
+with open(data) as file:
+    for line in file:
+        valid = True
+        info = {"red": 0, "green": 0, "blue": 0}
+        game_id = int(line.split(":")[0].split(" ")[1])
+        values = line.split(":")[1].strip().split(";")
+        for trial in values:
+            sets = trial.split(",")
+            for set in sets:
+                number, colour = set.strip().split(" ")
+                if info[colour] < int(number):
+                    info[colour] = int(number)
+        for colour, number in info.items():
+            if limits[colour] < number:
+                valid = False
+        if valid:
+            id_sum += game_id
+
+    print(id_sum)
